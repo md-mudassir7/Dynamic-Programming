2641e616be5198239ddf95a5fc5007cc165a32b8
mdmudassir7 <mohammedmudassir200@gmail.com>
2022-04-02 20:37:15 +0530

    Add buyAndSellStockIII

--- State Machine/buyAndSellStockIII/README.md
+++ State Machine/buyAndSellStockIII/README.md
@@ -1,4 +1,4 @@
-Approach
+Approach(class Solution1)
 
     1.This is similar to buyAndSellStockII but we are restricted to just 2 transactions
 
@@ -11,3 +11,9 @@ Approach
     5.If we can buy then we can decide to either buy or not buy,if we decide to buy then we update canBuy to 0 since we should sell this buy before buying again and noOfTransactionRemaining reamins constant as a transaction can only be complete if we have sold the stock hence it remains the same and we move to the next index, if we decide not to buy then canBuy and noOfTransactionRemaining will still be the same and we move to the next index
 
     6.If we cannot buy then we can decide to either sell or not sell,if we decide to sell then we update canBuy to 1 since we can buy the stock now and decrease noOfTransactionRemaining by 1 since one transaction is complete and we move to the next index,if we decide not to sell then canBuy and noOfTransactionRemaining will still be the same i.e we can look for some other day to sell the stock
+
+Approach2(class Solution2)
+
+    1.We use 4 variables first_buy,first_sell,second_buy,second_sell
+
+    2.The code gives clear understanding of the above approach
--- State Machine/buyAndSellStockIII/code.cpp
+++ State Machine/buyAndSellStockIII/code.cpp
@@ -25,3 +25,26 @@ public:
         return fun(0,1,2,prices,dp);
     }
 };
+
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int first_buy=INT_MIN,second_buy=INT_MIN;
+        int first_sell=0, second_sell = 0;
+        for(int i=0;i<n;i++){
+            //Since we do not have anything with us we are investing prices[i] for first_buy hence the negative sign
+            first_buy = max(first_buy,-1*prices[i]);
+            //We have bought the first stock and now we are selling it hence we are gaining amount of value = prices[i]
+            first_sell = max(first_sell,first_buy+prices[i]);
+            //We have the profit of first transaction with us now i.e stored in the variable first_buy and now we are investing prices[i] for second_buy hence the negative sign
+            second_buy = max(second_buy,first_sell-prices[i]);
+            //We have bought the second stock and now we are selling it hence we are gaining amount of value = prices[i]
+            second_sell = max(second_sell,second_buy+prices[i]);
+
+            //We are maximimizing all of the above values to get the maximu  profit of those 2 transactions
+            //At the end the profit we gain will be stored in second_sell variable
+        }
+        return second_sell;
+    }
+};
