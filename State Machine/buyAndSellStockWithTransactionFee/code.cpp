11bde154178f0823becee91d5791fc74d2a01f44
mdmudassir7 <mohammedmudassir200@gmail.com>
2022-04-04 18:22:28 +0530

    Add buyAndSellStockWithTransactionFee

--- State Machine/buyAndSellStockWithTransactionFee/code.cpp
+++ State Machine/buyAndSellStockWithTransactionFee/code.cpp
@@ -72,3 +72,29 @@ public:
         return dp[0][1];
     }
 };
+
+class Solution3 {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<int> next(2,0);
+        vector<int> current(2,0);
+        next[0]=0;
+        next[1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int buy = 0;buy<=1;buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max(-prices[i]+next[0],next[1]);
+                }
+                else
+                {
+                    profit = max(prices[i]-fee+next[1],next[0]);
+                }
+                current[buy]=profit;
+            }
+            next = current;
+        }
+        return current[1];
+    }
+};
