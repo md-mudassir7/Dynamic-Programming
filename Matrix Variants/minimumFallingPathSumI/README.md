7f6a9eca011093462da98dc6b8cb1229dd25c17c
mdmudassir7 <mohammedmudassir200@gmail.com>
2022-04-08 16:23:39 +0530

    Add minimumFallingPathSumI

---
+++ Matrix Variants/minimumFallingPathSumI/README.md
@@ -0,0 +1,13 @@
+Problem [link](https://leetcode.com/problems/minimum-falling-path-sum/)
+
+Approach
+
+    1.The problem is similar to maximal square
+
+    2.We create a dp array of size (n+1)x(n+1) 
+
+    3.At any index in the matrix the value dp[i][j] denotes the minimum falling path sum from top to that point (i,j)
+
+    4.dp[i][j] is the minimum of 3 values(top diagonally left,top diagonally right and the top value)
+
+    5.At the end we return the minimum value stored in the last row of dp matrix
---
+++ Matrix Variants/minimumFallingPathSumI/code.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int dp[n+1][n+1];
+        for(int i=0;i<=n;i++)
+        {
+            dp[0][i] = INT_MAX;
+            dp[i][0] = INT_MAX;
+        }
+        for(int i=1;i<=n;i++){
+            dp[1][i] = matrix[0][i-1];
+        }
+        
+        for(int i=2;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                dp[i][j] = matrix[i-1][j-1]+min(min(dp[i-1][j],dp[i-1][j-1]),dp[i-1][j+1]);
+            }
+        }
+        int mini = dp[n][1];
+        for(int i=2;i<=n;i++)
+        {
+            mini = min(mini,dp[n][i]);
+        }
+        return mini;
+    }
+};
